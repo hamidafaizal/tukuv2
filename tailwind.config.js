57c0a9ff61775abc234685a370d60d6403cf28f9
hamidafaizal <hamidafaizalamin0@gmail.com>
2025-09-17 12:07:15 +0700

    2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  // Menentukan file yang akan dipindai oleh Tailwind untuk kelas CSS.
+  // Ini termasuk file HTML dan JSX di dalam folder `src` dan `public`.
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
